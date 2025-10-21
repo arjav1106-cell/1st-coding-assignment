f89af800228163a1485c768344b1a7aae66881d7
Arjav Jain <arjav.dev1106gmail,com>
2025-10-21 12:14:17 +0530

    Changed the name of the file to OrdinalEncoding

---
+++ OrdinalEncoding.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+x = pd.read_csv("./Git programs/AIMS/1st-coding-assignment/sales_forecast_sample_expanded.csv")
+x_test = pd.read_csv("./Git programs/AIMS/1st-coding-assignment/sales_forecast_test.csv")
